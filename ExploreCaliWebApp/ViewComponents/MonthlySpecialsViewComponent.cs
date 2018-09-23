312be6b19635487c2311f082bc8ef224da32f31c
Cem Calgici <cemx0009@edu.eal.dk>
2018-09-24 00:21:29 +0200

    new class

---
+++ ExploreCaliWebApp/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCaliWebApp.ViewComponents
+{
+    
+   public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
