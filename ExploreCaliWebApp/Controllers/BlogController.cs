20e29c7e47b43c999cd10a3b7af22f8d2051f98f
Cem Calgici <Cemox35@live.dk>
2018-09-23 23:49:58 +0200

    123

---
+++ ExploreCaliWebApp/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExploreCaliWebApp.Models;
+
+namespace ExploreCaliWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Cem Calgici",
+                    Body = "heheheheheh",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Cem Calgici",
+                    Body = "heheheheheheh xD",
+                },
+            };
+
+            return View(posts);
+
+        }
+
+        public IActionResult Post(int year, int month, string key)
+        {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Cem Calgici",
+                Body = "heheheheheheh",
+            };
+            return View(post);
+        }
+    }
+}
---
+++ ExploreCaliWebApp/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCaliWebApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ExploreCaliWebApp
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+                  configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile(env.ContentRootPath + "/config.json")
+                .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                .Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FormattingService>();
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            FeatureToggles features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+           
+            //if (configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")) 
+            if (features.EnableDeveloperExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes => { routes.MapRoute("Default", "{controller=Home}/{action=index}/{id?}"); });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("Error!");
+
+                await next();
+
+
+            });
+
+            app.UseFileServer();
+        }
+    }
+}
+ 
