8372d9aaff29ad1e3488ba4a41be8eca1d7eebf4
Cem Calgici <Cemox35@live.dk>
2018-10-01 11:27:15 +0200

    123

---
+++ ExploreCaliWebApp/Models/RandomFactsDataContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCaliWebApp.Models
+{
+    public class RandomFacts
+    {
+        public string Key { get; set; }
+        public string Fact { get; set; }
+
+        public string Refresh = "Refresh to see other facts";
+
+    }
+
+    public class RandomFactsDataContext
+    {
+        public IEnumerable<RandomFacts> GetRandomFacts()
+        {
+            return new[]
+            {
+                new RandomFacts {Key = "Første Fact" , Fact = "2+2=4" },
+                new RandomFacts {Key = "Anden Fact" , Fact = "Dette er dynamisk indhold"},
+                new RandomFacts {Key =  "Tredje Fact", Fact = "Hunde > Katte"} 
+
+            };
+
+
+
+        }
+    }
+    
+}
---
+++ ExploreCaliWebApp/ViewComponents/RandomFactsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCaliWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCaliWebApp.ViewComponents
+{
+    public class RandomFactsDataContext : ViewComponent
+    {
+        private readonly RandomFactsDataContext _facts;
+
+        public RandomFactsViewComponent(RandomFactsDataContext randomFacts)
+        {
+            _facts = randomFacts;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var facts = _facts.GetRandomFacts();
+            return View(facts);
+        }
+    
+}
